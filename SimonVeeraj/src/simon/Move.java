4d86a4e321cf1d3058e3b31bf847547e1edc8561
Student8 <Student8@10.8.33.167>
2017-01-09 13:11:58 -0500

    asdasd

---
+++ SimonVeeraj/src/simon/Move.java
@@ -0,0 +1,15 @@
+package simon;
+
+public class Move implements MoveInterfaceVeeraj {
+
+	public Move() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public ButtonInterfaceVeeraj getButton() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
